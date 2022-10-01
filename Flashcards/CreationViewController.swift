e64a195bb0993560b814eb5b3e6c01a7ab53674d
crowley-ce <106932285+crowley-ce@users.noreply.github.com>
2022-10-01 13:46:11 -0400

    Flashcards lab 2 done

---
+++ Flashcards/CreationViewController.swift
@@ -0,0 +1,77 @@
+//
+//  CreationViewController.swift
+//  Flashcards
+//
+//  Created by Christopher Crowley on 9/22/22.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+
+    var flashcardsController: ViewController!
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var extraAnswerOne: UITextField!
+    @IBOutlet weak var extraAnswerTwo: UITextField!
+    
+    
+    var initialQuestion: String?
+    var initialAnswer: String?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        questionTextField.text = initialQuestion
+        answerTextField.text = initialAnswer
+    }
+    
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+       
+        //Get the text in the question text field
+        let questionText = questionTextField.text
+        
+        //Get the text in the answer text field
+        let answerText = answerTextField.text
+        
+        //Get the texts in the extra answer text fields
+        let extraAnswerOneText = extraAnswerOne.text
+        
+        let extraAnswerTwoText = extraAnswerTwo.text
+        
+        let alert = UIAlertController(title: "Missing text", message: "You need to enter both a question and an answer", preferredStyle: .alert)
+       
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        
+        //Check if empty
+        if (questionText == nil || answerText == nil || questionText!.isEmpty || answerText!.isEmpty) {
+            //show error
+            present(alert, animated: true)
+        }
+        else {
+            //Call the function to update the flashcard
+            flashcardsController.updateFlashcard(question: questionText!, answer: answerText!, extraAnswerOne: extraAnswerOneText!, extraAnswerTwo: extraAnswerTwoText)
+        
+            //Dismiss
+            dismiss(animated: true)
+        }
+    }
+        
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
+
--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,11 +9,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var card: UIView!
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        frontLabel.layer.cornerRadius = 20.0
+        frontLabel.clipsToBounds = true
+        frontLabel.backgroundColor = UIColor(patternImage: UIImage(named: "paper")!)
+        backLabel.layer.cornerRadius = 20.0
+        backLabel.clipsToBounds = true
+        backLabel.backgroundColor = UIColor(patternImage: UIImage(named: "answerpaper")!)
+        
+        btnOptionOne.layer.borderWidth = 3.0
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        btnOptionOne.layer.cornerRadius = 20.0
+        btnOptionTwo.layer.borderWidth = 3.0
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        btnOptionTwo.layer.cornerRadius = 20.0
+        btnOptionThree.layer.borderWidth = 3.0
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        btnOptionThree.layer.cornerRadius = 20.0
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //We know the destination of the segue is the Navigation Controller
+        let navigationController = segue.destination as! UINavigationController
+        
+        //We know the Navigation Controller only contains a Creation View Controller
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        //We set the flashcardsController property to self
+        creationController.flashcardsController = self
+        
+        if segue.identifier == "EditSegue" {
+            creationController.initialQuestion = frontLabel.text
+            creationController.initialAnswer = backLabel.text
     }
 
 
 }
 
+    @IBAction func didTapFlashcard(_ sender: Any) {
+        frontLabel.isHidden = !frontLabel.isHidden
+    }
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true
+    }
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        btnOptionThree.isHidden = true
+    }
+    func updateFlashcard(question: String, answer: String, extraAnswerOne: String?, extraAnswerTwo: String?) {
+        frontLabel.text = question
+        backLabel.text = answer
+        
+        btnOptionOne.setTitle(extraAnswerOne, for: .normal)
+        btnOptionTwo.setTitle(answer, for: .normal)
+        btnOptionThree.setTitle(extraAnswerTwo, for: .normal)
+        
+        }
+}
